d4b92f7fbc1604d6b63e48cd5ea5656867124646
shalini004 <77494427+shalini004@users.noreply.github.com>
2021-02-20 19:55:38 +0530

    Update P1_Pairwise_Alignment.py

--- P1_Pairwise_Alignment.py
+++ P1_Pairwise_Alignment.py
@@ -3,30 +3,55 @@ se2=input("Enter the second sequence::")
 seq1=list(se1)
 seq2=list(se2)
 score=[]
+
 def Pairwise_alignment(a,b):
     gap(a,b)
     print(a)
     print(b)
     value=0
     length=len(a)
-    for i in range(0,length):
-        if(a[i]==b[i]):
+    for i in range(0, length):
+        if (a[i]==b[i]):
             score.append('1')
             value=value+1
         else:
             score.append('0')
-    print(score)
-    print(value)
-    
+        print(score)
+        print(value)
+
 def gap(a,b):
-    if(len(a)==len(b)):
+    if (len(a)==len(b)):
         print()
     else:
         k=int(input("Enter the position to insert::"))
-        if(len(a)<len(b)):
+        if (len(a)<len(b)):
             a.insert(k,'-')
         else:
             b.insert(k,'-')
     return(a,b)
-
 Pairwise_alignment(seq1,seq2)
+
+
+//output:
+Enter the first sequence::ACTGACTG
+Enter the second sequence::ACTGGTCA
+
+['A', 'C', 'T', 'G', 'A', 'C', 'T', 'G']
+['A', 'C', 'T', 'G', 'G', 'T', 'C', 'A']
+['1']
+1
+['1', '1']
+2
+['1', '1', '1']
+3
+['1', '1', '1', '1']
+4
+['1', '1', '1', '1', '0']
+4
+['1', '1', '1', '1', '0', '0']
+4
+['1', '1', '1', '1', '0', '0', '0']
+4
+['1', '1', '1', '1', '0', '0', '0', '0']
+4
+> 
